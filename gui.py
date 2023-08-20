c30a14a8ef69abe9e78f4969a3e1672fd4d841aa
Bopster411 <serg.dr2004@gmail.com>
2023-08-20 12:33:54 +0300

    Added entry fields to the dialog

--- gui.py
+++ gui.py
@@ -21,13 +21,29 @@ class DialogWindow(tk.Toplevel):
         # Make other windows not clickable
         self.grab_set()
 
-        self._user_input = namedtuple('Input', ['min_val', 'max_val'])(1000, 10000)
+        self._Input = namedtuple('Input', ['min_val', 'max_val'])
+        self._user_input = None
     
     def _create_widgets(self):
+        self._min_val_lbl = tk.Label(self, text='Min value:')
+        self._min_val_lbl.grid(row=0, column=0)
+
+        self._min_val_entry = tk.Entry(self)
+        self._min_val_entry.grid(row=1, column=0)
+        
+        self._max_val_lbl = tk.Label(self, text='Min value:')
+        self._max_val_lbl.grid(row=2, column=0)
+
+        self._max_val_entry = tk.Entry(self)
+        self._max_val_entry.grid(row=3, column=0)
+
         self._confirm_input_btn = tk.Button(self, text='Confrm', command=self._confirm_input)
-        self._confirm_input_btn.grid(row=0, column=0)
+        self._confirm_input_btn.grid(row=4, column=0)
     
     def _confirm_input(self):
+        min_val = int(self._min_val_entry.get())
+        max_val = int(self._max_val_entry.get())
+        self._user_input = self._Input(min_val=min_val, max_val=max_val)
         self.grab_release()
         self.destroy()
 
@@ -52,7 +68,6 @@ class ExprApp(tk.Tk):
         self.update_score()
         self.score_label.grid(row=0, column=0, padx=5, pady=5)
 
-
         self.string = tk.StringVar(value='hello!!!')
         self.label = tk.Label(self, textvariable=self.string, bg='#252526', fg='#b6bbc0',font=('Arial', 30))
         self.label.grid(row=1, column=0, sticky='nswe')
@@ -64,8 +79,7 @@ class ExprApp(tk.Tk):
         self.bind('<Return>', lambda e: self.enter_cmd())
         
         input = DialogWindow(self).get_input()
-        # TODO get min/max values from window before the test 
-        min_value, max_value = input
+        min_value, max_value = input.min_val, input.max_val
         self.expression = Expression(min_value=min_value, max_value=max_value)
         self.update_expr()
     
