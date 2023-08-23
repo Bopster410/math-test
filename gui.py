7cdca5d143474cdfc0c94bd08969f3d34b1f39d1
Bopster411 <serg.dr2004@gmail.com>
2023-08-23 10:49:02 +0300

    Added menu button

--- gui.py
+++ gui.py
@@ -58,22 +58,26 @@ class ExprApp(tk.Tk):
         self.geometry('800x800')
         self.configure(bg='#252526')
         self.rowconfigure(1, weight=1)
-        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
 
         self.score = 0
         self.high_score = 0
 
+        self.menu_img = tk.PhotoImage(file='menu_img.png')
+        self.menu_btn = tk.Button(self, image=self.menu_img, bg='#252526', fg='#252526', activebackground='#4a4a4c', borderwidth=0,command=self.open_menu)
+        self.menu_btn.grid(row=0, column=0)
+
         self.score_string = tk.StringVar()
         self.score_label = tk.Label(self, textvariable=self.score_string, bg='#252526', fg='#b6bbc0', font=('Arial', 15))
         self.update_score()
-        self.score_label.grid(row=0, column=0, padx=5, pady=5)
+        self.score_label.grid(row=0, column=1, padx=(0, 40), pady=5)
 
         self.string = tk.StringVar(value='hello!!!')
         self.label = tk.Label(self, textvariable=self.string, bg='#252526', fg='#b6bbc0',font=('Arial', 30))
-        self.label.grid(row=1, column=0, sticky='nswe')
+        self.label.grid(row=1, column=0, columnspan=2, sticky='nswe')
         
         self.input = tk.Entry(self,bg='#333333', fg='#b6bbc0', insertbackground='white', font=('Arial', 20))
-        self.input.grid(row=2, column=0, sticky='nswe')
+        self.input.grid(row=2, column=0, columnspan=2, sticky='nswe')
         self.input.focus_set()
 
         self.bind('<Return>', lambda e: self.enter_cmd())
@@ -104,6 +108,9 @@ class ExprApp(tk.Tk):
         
         self.update_score()
         self.input.delete(0, len(answer))
+    
+    def open_menu(self):
+        pass
 
 if __name__ == '__main__':
     e = ExprApp()
